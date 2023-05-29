08c54c4a4148bc487652144820646bb29fccef90
Viet Quang Vu <vuvietquang90@gmail.com>
2023-05-29 14:11:22 +0700

    change db connection

--- scraping_cooky.py
+++ scraping_cooky.py
@@ -44,9 +44,9 @@ driver.get("https://www.cooky.vn/cach-lam/")
 
 # Category dictionary
 xpath = {
-    'Thực đơn': '/html/body/div[10]/div/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div[1]/div',
+    # 'Thực đơn': '/html/body/div[10]/div/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div[1]/div',
     # 'Loại món': '/html/body/div[10]/div/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div[2]/div',
-    # 'Nguyên liệu': '/html/body/div[10]/div/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div[3]/div',
+    'Nguyên liệu': '/html/body/div[10]/div/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div[3]/div',
     # 'Độ khó': '/html/body/div[10]/div/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div[4]/div',
     # 'Ẩm thực': '/html/body/div[10]/div/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div[6]/div',
     # 'Cách thực hiện': '/html/body/div[10]/div/div[1]/div/div/div/div/div[2]/div[1]/div[2]/div[7]/div'
@@ -68,12 +68,15 @@ conn = db.connection
 cur = conn.cursor()
 
 for parent_name, xpath_value in xpath.items():
-    food_categories = ['Thức uống']
-    gray_span = driver.find_element(
-        By.XPATH, "//strong[@class='text-highlight ng-binding']")
-    gray_text = gray_span.text
-    gray_num = int(re.findall('\d+', gray_text)[0])
-    print(gray_num)
+    # food_categories = ['Cá', 'Tôm tươi CP Fresh Mart', 'Tôm', 'Cua', 'Ghẹ', 'Mực - Bạch tuộc',
+                    #    'Ốc', 'Sò', 'Hàu', 'Bào ngư', 'Loại thủy hải sản khác', 'Chuột', 'Heo', 'Bò', 'Thịt nạc vai CP Fresh Mart', 'Gà', 'Vịt', 'Dê', 'Ếch', 'Chim',  'Cầy', 'Cừu', 'Thỏ', 'Mèo', 'Bò Sát', 'Trâu', 'Nai', 'Ngỗng']
+    # insuf_food_categories = ['Bò', 'Vịt','Dê','Thỏ','Cừu', 'Bò Sát', 'Trâu', 'Rau', 'Củ', 'Quả', 'Loại bột khác', 'Tinh bột', 'Ngũ cốc']
+    food_categories = ['']
+
+    food_categories = [category for category in food_categories if category not in insuf_food_categories]
+    
+    # if parent_name in common_food_categories:
+    #     food_categories = food_categories + insuf_food_categories
     parent_id = int(
         ((xpath_value.split('/')[13]).replace('div', '')).replace('[', '').replace(']', ''))
 
@@ -86,17 +89,20 @@ for parent_name, xpath_value in xpath.items():
         By.XPATH, '//span[@class="fa text-gray fa-square-o"]')
     for submenu in submenus:
         submenu.click()
+        print('clicked submenu')
         count += 1
         # extract the value from span element
         selected_submenu = driver.find_element(
             By.XPATH, '//span[@class="fa text-gray fa-check-square text-highlight"]/following-sibling::span[@class="ng-binding"]')
         submenu_text = selected_submenu.text
         # restrict conditions to  only those submenus that are or aren't in the food_categories list
-        if submenu_text in food_categories:
+        if submenu_text not in food_categories:
             submenu_dict[submenu_text] = []
             print(f"  checked {submenu_text}")
+            
 
         # if there is something to load, then load more
+            time.sleep(5)
             red_span = driver.find_element(
                 By.XPATH, "//span[@class='text-red ng-binding']")
             gray_span = driver.find_element(
@@ -106,36 +112,74 @@ for parent_name, xpath_value in xpath.items():
             gray_text = gray_span.text
 
             # Extract the numbers from the text using regular expressions
-            red_num = int(re.findall('\d+', red_text)[0])
-            gray_num = int(re.findall('\d+', gray_text)[0])
+            try:
+                red_num = int(re.findall('\d+', red_text)[0])
+                gray_num = int(re.findall('\d+', gray_text)[0])
+            except IndexError:
+                submenu.click()
+                continue  # Exit the loop if red_num or gray_num is not found
 
             # Click the "Xem thêm" button repeatedly until there are no more recipes to load
             try:
                 while True:
-                    # print(f"{red_num}/{gray_num}")
-                    load_more_button = WebDriverWait(driver, WEBPAGE_TIMEOUT).until(EC.element_to_be_clickable(
-                        (By.XPATH, '/html/body/div[10]/div/div[1]/div/div/div/div/div[4]/div[4]/div/a/span[1]')))
+                    load_more_button = WebDriverWait(driver, WEBPAGE_TIMEOUT).until(
+                        EC.element_to_be_clickable(
+                            (By.XPATH,
+                             '/html/body/div[10]/div/div[1]/div/div/div/div/div[4]/div[4]/div/a/span[1]')
+                        )
+                    )
                     load_more_button.click()
+
+                    # Introduce a delay to allow the page to load fully
+                    time.sleep(3)  # Adjust the delay duration as needed
+
+                    # Wait for all the items to be loaded fully
+                    # WebDriverWait(driver, WEBPAGE_TIMEOUT).until(
+                    #     EC.visibility_of_all_elements_located(
+                    #         (By.XPATH, '//div[@ng-repeat="recipe in recipes"]')
+                    #     )
+                    # )
+
+                    # Check if the "Load More" button is disabled
+                    if not load_more_button.is_enabled():
+                        break
             except:
                 # Wait for the page to load and extract the new recipe links
                 html = driver.page_source
-                soup = BeautifulSoup(html, 'html.parser')
-                # recipe_link as a set
-                for a_tag in soup.find_all('a', href=True):
-                    href = a_tag['href']
-                    if href.startswith('/cong-thuc'):
-                        food_id = int(''.join(filter(str.isdigit, href)))
-                        # add mapping to the dict only if the food_id is not already in the set
-                        if food_id not in food_id_set:
-                            # add the mapping to the food_id_dict
-                            submenu_dict[submenu_text].append(food_id)
-                            food_id_set.add(food_id)
-                            # print(food_id)
+                file_path = r'G:\\0_9Health\\6_data\\Food-RS\\offline_pages\\cooky\\test-1.html'
+                with open(file_path, 'w', encoding='utf-8') as file:
+                    file.write(html)
 
-                # create a list of FoodCategory objects
+                # Read the HTML file and create a BeautifulSoup object
+                with open(file_path, 'r',  encoding='utf-8') as file:
+                    html_content = file.read()
+
+                soup = BeautifulSoup(html_content, 'html.parser')
+                span_element = soup.find('span', class_='text-red ng-binding')
+                text = span_element.get_text(strip=True)
+                print(text)
+
+                # Find all <div> tags with ng-repeat="recipe in recipes"
+                recipe_divs = soup.find_all(
+                    'div', attrs={'ng-repeat': 'recipe in recipes'})
+                # Iterate over each recipe_div and extract href values from <a> tags
+                for recipe_div in recipe_divs:
+                    links = recipe_div.select('a.photo[rel="alternate"]')
+                    hrefs = [link['href'] for link in links]
+                    for href in hrefs:
+                        if href.startswith('/cong-thuc'):
+                            food_id = int(''.join(filter(str.isdigit, href)))
+                            # add mapping to the dict only if the food_id is not already in the set
+                            if food_id not in food_id_set:
+                                # add the mapping to the food_id_dict
+                                submenu_dict[submenu_text].append(food_id)
+                                food_id_set.add(food_id)
+                                # print(food_id)
+
+            # create a list of FoodCategory objects
             for submenu_text, food_ids in submenu_dict.items():
                 # restrict conditions to  only those submenus that are or aren't in the food_categories list
-                if submenu_text in food_categories:
+                if submenu_text not in food_categories:
                     if food_ids:
                         print(len(food_ids))
                         for food_id in food_ids:
@@ -149,7 +193,7 @@ for parent_name, xpath_value in xpath.items():
 
                             # Insert into database
                             cur.execute(
-                                "INSERT INTO foods_category_cooky_test (parent_category_id, parent_category_name, child_category_id, child_category_name, food_id) VALUES (%s, %s, %s, %s, %s)",
+                                "INSERT INTO foods_category_cooky (parent_category_id, parent_category_name, child_category_id, child_category_name, food_id) VALUES (%s, %s, %s, %s, %s)",
                                 (
                                     food_category.parent_id,
                                     food_category.parent_name,
@@ -159,7 +203,7 @@ for parent_name, xpath_value in xpath.items():
                                 )
                             )
                             conn.commit()
-            food_categories.append(food_category.child_name)
+                            food_categories.append(food_category.child_name)
         submenu.click()  # Close submenu
         print(f"  unchecked {submenu_text}")
     element.click()
